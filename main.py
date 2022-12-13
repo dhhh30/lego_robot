3d0405cdca7e62d83dd2a82742926d2e5d98ce6a
Yurun Chen <chendylan680@gmail.com>
2022-12-13 13:17:40 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,45 @@
 #!/usr/bin/env pybricks-micropython
-
-"""
-Example LEGO® MINDSTORMS® EV3 Robot Educator Color Sensor Down Program
-----------------------------------------------------------------------
-
-This program requires LEGO® EV3 MicroPython v2.0.
-Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
-
-Building instructions can be found at:
-https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#robot
-"""
-
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, ColorSensor
-from pybricks.parameters import Port
-from pybricks.tools import wait
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+ 
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click
 
-# Initialize the motors.
-left_motor = Motor(Port.B)
-right_motor = Motor(Port.C)
+# Create your objects here.
 ev3 = EV3Brick()
-# Initialize the color sensor.
-line_sensor = ColorSensor(Port.S3)
+left = Motor (Port.B)
+right = Motor(Port.C)
+arm = Motor (Port.A)
+robot = DriveBase(left, right, wheel_diameter=80, axle_track=120)
+speed = 40
+steering= 0
+time = 500
+ 
+light = ColorSensor (Port .S1)
+ultra = UltrasonicSensor(Port.S2)
+arm.run(450)
+# Write your program here.
+while light.color() != Color.RED:
+    correction = (-25 + light.reflection ())*2.5
+    robot. drive(speed, correction)
+    if ultra.distance() < 50:
+        robot. turn(bee)
+    if 90 > light.reflection() > 60:
+        robot. stop()
+    if light.color() == Color.GREEN:
+        for i in range(18):
+            robot.turn(10)
+            if light.color() == Color.BLACK:
+                for i in range(i):
+                    robot.turn(-10)
+            
+                    
+                    
 
-# Initialize the drive base.
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
-
-# Calculate the light threshold. Choose values based on your measurements.
-BLACK = 9
-WHITE = 85
-threshold = (BLACK + WHITE) / 2
-
-# Set the drive speed at 100 millimeters per second.
-DRIVE_SPEED = 100
-
-# Set the gain of the proportional line controller. This means that for every
-# percentage point of light deviating from the threshold, we set the turn
-# rate of the drivebase to 1.2 degrees per second.
-
-# For example, if the light value deviates from the threshold by 10, the robot
-# steers at 10*1.2 = 12 degrees per second.
-PROPORTIONAL_GAIN = 1.2
-class operations(EV3Brick):
-    def __init__(self):
-        self.ev3 = super.EV3Brick()
-    def get_turn_rate(PROPORTIONAL_GAIN, deviation):
-        turn_rate = PROPORTIONAL_GAIN * deviation
-        return turn_rate
-    def drive (DRIVE_SPEED, turn_rate):
-        robot.drive(DRIVE_SPEED, turn_rate)
-    def deviation():
-        deviation = line_sensor.reflection() - threshold
-        return deviation
-# Start following the line endlessly.
-op = operations
-while True:
-    op.drive(DRIVE_SPEED, op.get_turn_rate(PROPORTIONAL_GAIN, int(op.deviation)))
+        
+        
